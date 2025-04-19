a62e5dca41b6bd2ebd98f60530f99cebd2999872
osmanx8 <ismael.venderk@gmail.com>
2025-04-19 22:06:14 +0900

    set configration

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 const dotenv = require('dotenv')
 
-// Load env vars if env is not production
 if (process.env.NODE_ENV !== 'production') {
   dotenv.config({ path: './server/config/local.env' })
 }
